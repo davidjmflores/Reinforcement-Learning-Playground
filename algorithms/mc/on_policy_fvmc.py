a86bf2b91accb1d6d40b3388e6fea1fa3ec2fb1a
davidjmflores <davidjmflores@gmail.com>
2026-02-17 23:19:08 -0700

    Fixed to be able to work w/ Windy gridworld.

--- algorithms/mc/on_policy_fvmc.py
+++ algorithms/mc/on_policy_fvmc.py
@@ -3,48 +3,41 @@
 # Policy used for policy improvement in PI, VI, and MCES
 
 class EpsilonSoftPolicy:
-    def __init__(self, env, epsilon, rng):
+    def __init__(self, env, epsilon):
         self.env = env
         self.epsilon = epsilon
         self.pi_table = {}
 
-        for s in env.states():
-            actions = list(env.actions(s))
-            if not actions:
-                self.pi_table[s] = {}
-                continue
-            
-            # set all actions to have p = epsilon / |A(s)|
-            p = self.epsilon / len(actions)
-            self.pi_table[s] = {a: p for a in actions}
+    def _ensure_state(self, s):
+        if s in self.pi_table:
+            return
+        actions = list(self.env.actions(s))
+        self.pi_table[s] = {a: 1.0/len(actions) for a in actions} if actions else {}
 
-            # Randomly select an initial greedy action
-            a_greedy = rng.choice(list(actions))
-            self.pi_table[s][a_greedy] += 1.0 - self.epsilon
+    def sample(self, rng, s):
+        self._ensure_state(s)
+        dist = self.pi_table[s]
+        actions = list(dist.keys())
+        probs = [dist[a] for a in actions]
+        idx = rng.choice(len(actions), p=probs)
+        return actions[idx]
 
-    def pi(self, s):
-        return self.pi_table[s]
-
-    def set_epsilon_greedy(self, s, greedy_actions):
-        if not greedy_actions:
-            raise ValueError(f"set_greedy called with empty greedy_actions for state {s}")
-
+    def make_epsilon_greedy(self, s, greedy_actions):
+        self._ensure_state(s)
         actions = list(self.env.actions(s))
         if not actions:
             self.pi_table[s] = {}
             return
+        if not greedy_actions:
+            raise ValueError(f"empty greedy_actions for state {s}")
 
-        # assign prob to non-greedy actions as (epsilon) / |actions(s)|
-        p = self.epsilon / len(actions)
-        self.pi_table[s] = {a: p for a in actions}
-
-        # assign prob to greedy actions as (1 - epsilon) / |greedy_actions(s)|
-        p = (1 - self.epsilon) / len(greedy_actions)
+        base = self.epsilon / len(actions)
+        dist = {a: base for a in actions}
+        bonus = (1.0 - self.epsilon) / len(greedy_actions)
         for a in greedy_actions:
-            if a not in self.pi_table[s]:
-                raise ValueError(f"Greedy action {a} not legal in state {s}")
-            self.pi_table[s][a] += p
-            
+            dist[a] += bonus
+        self.pi_table[s] = dist
+
 class OnPolicyFVMC:
     def __init__(self, env, epsilon, gamma, tolerance, rng):
         self.env = env
@@ -52,49 +45,39 @@ class OnPolicyFVMC:
         self.gamma = gamma
         self.tolerance = tolerance
         self._states = self.env.states()
+        self.total_steps = 0
+        self.episode_end_steps = []
+        self.rng = rng
 
-        self.policy = EpsilonSoftPolicy(self.env, epsilon, rng)
+        self.policy = EpsilonSoftPolicy(self.env, epsilon)
         self.Q = {s: {a: 0.0 for a in self.env.actions(s)} for s in self._states}
         self.N = {s: {a: 0 for a in self.env.actions(s)} for s in self._states}
-
-    # Takes a sample action from pi(a|s)
-    def policy_sample(self, s, rng):
-        actions = list(self.env.actions(s))
-        if not actions:
-            raise ValueError(f"No actions available in state {s}")
-        
-        dist = self.policy.pi(s)
-        probs = [dist.get(a, 0.0) for a in actions]
-        total = sum(probs)
-        if total <= 0: probs = [1.0 / len(actions)] * len(actions)
-        else: probs = [p / total for p in probs]
-        return rng.choice(actions, p=probs)
     
-    def episode(self, rng):
+    def episode(self):
         episode = []
 
-        s0, r0, done = self.env.reset(rng)
-        
-        if done:
-            episode.append((s0, None, r0))
-            return episode
+        s0, _ = self.env.reset()
         
         s = s0
-        a = self.policy_sample(s, rng)
+        a = self.policy.sample(self.rng, s)
 
         while True:
-            s_prime, r, done = self.env.step(s, a, rng)
+            s_prime, r, terminated, truncated, _ = self.env.step(a)
+            done = terminated or truncated
             episode.append((s, a, r))
 
-            if done: break
+            self.total_steps += 1
+            if done: 
+                self.episode_end_steps.append(self.total_steps)
+                break
 
             s = s_prime
-            a = self.policy_sample(s, rng)
+            a = self.policy.sample(self.rng, s)
         return episode
     
-    def run(self, episodes, rng):
+    def run(self, episodes):
         for _ in range(episodes):
-            episode = self.episode(rng)
+            episode = self.episode()
             T = len(episode)
 
             G = 0.0
@@ -130,15 +113,6 @@ class OnPolicyFVMC:
                 greedy_actions = [a for a in legal_actions
                                   if abs(self.Q[s_t][a] - max_q) < self.tolerance]
                 
-                self.policy.set_epsilon_greedy(s_t, greedy_actions)
+                self.policy.make_epsilon_greedy(s_t, greedy_actions)
         
-        return self.Q, self.policy
-
-
-    
-    
-
-    
-
-
-
+        return self.Q, self.policy, self.episode_end_steps
