5ee94f6f4a85ea8ab1154feeba7d389ee51b6040
davidjmflores <davidjmflores@gmail.com>
2026-02-18 22:50:12 -0700

    Improved windy gridworld to handle king's moves, stay-in-place, and stochastic winds.

--- environments/ch6_windy_gridworld.py
+++ environments/ch6_windy_gridworld.py
@@ -1,15 +1,11 @@
-# Normal gridworld 7x10 except that there is a crosswind running upwards
-# through the middle of the grid
-# columns:        [4, 5, 6, 7, 8, 9] 
-# wind intensity: [1, 1, 1, 2, 2, 1]
-# actions: [up, down, left, right]
-# In the wind-effect states, next states are shifted upward by the wind
-# Undiscounted episodic task, w/ constant r = -1 until goal reached.
-
+import numpy as np
 class WindyGridworld:
     def __init__(self):
         self._rows, self._cols = 7, 10
-        self._actions = [(-1, 0), ( 1, 0), ( 0,-1), ( 0, 1)]
+        self._actions = [(-1, 0), ( 1, 0), ( 0,-1), ( 0, 1), # up, down, left, right
+                         (-1,-1), (-1, 1), ( 1,-1), ( 1, 1), # top-left, top-right, bottom-left, bottom-right
+                         ( 0, 0)] # stay still
+        
         self._wind_strength_by_col = [0, 0, 0, 1, 1, 1, 2, 2, 1, 0]
         self._start = (3, 0) # start position
         self._goal  = (3, 7)
@@ -19,20 +15,30 @@ class WindyGridworld:
         for r in range(self._rows):
             for c in range(self._cols):
                 self._states.append((r, c))
+
+        self.np_random = np.random.default_rng()
+
     
     def actions(self, s=None): return self._actions
 
     # Added for MC
     def states(self): return [(r, c) for r in range(self._rows) for c in range(self._cols)]
 
-    def reset(self): 
+    def reset(self, seed=None): 
+        if seed is not None: self.np_random = np.random.default_rng(seed)
         self._curr_state = self._start
         return self._start, {} # Gym-like returns (obs0, reset_info)
 
+    def wind_effect(self, col):
+        w = self._wind_strength_by_col[col]
+        if w == 0: return 0
+        dev = self.np_random.choice([-1, 0, 1])
+        return w + dev
+    
     def step(self, a): # state not part of step as compliance to gym
         r, c = self._curr_state
         dr, dc = a
-        wind = self._wind_strength_by_col[c]
+        wind = self.wind_effect(c)
         r_prime = min(self._rows - 1, max(0, r + dr - wind))
         c_prime = min(self._cols - 1, max(0, c + dc))
         
