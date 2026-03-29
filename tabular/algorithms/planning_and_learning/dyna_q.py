72c3553220280740a44015d8ff4bab3ddaf5ea59
davidjmflores <davidjmflores@gmail.com>
2026-03-29 16:24:31 -0600

    Added DynaQ and Ex. 8.1 from pg.164-165.

--- tabular/algorithms/planning_and_learning/dyna_q.py
+++ tabular/algorithms/planning_and_learning/dyna_q.py
@@ -1,19 +1,3 @@
-'''
-Init Q(s, a) and Model(s, a) for all s in S and a in A(s)
-Loop forever:
-    a. S gets current non-terminal state
-    b. A gets eps-greedy(S, Q)
-    c. Take action A; observe resultant R and S'
-    d. Q(s, a) += alpha * [R + gamma * max of a of Q(s', a) - Q(s, a)]
-    e. model(s, a) gets R and s' (assuming deterministic env)
-    f. Loop repeat n times:
-        s gets random previously observed state
-        a gets random action previously taken in s
-        r, s' get model(s, a)
-        Q(s, a) += alpha * [r + gamma * max over a of Q(s', a) - Q(s, a)]
-
-'''
-
 class EpsilonGreedy:
     def __init__(self, env, epsilon):
         self.env = env
@@ -25,63 +9,72 @@ class EpsilonGreedy:
 
         if rng.random() < self.epsilon: return actions[rng.integers(len(actions))]
 
-        q_vals = [Q.setdefault(s, {}).get(a, 0.0) for a in actions]
+        q_vals = [Q.get(s, {}).get(a, 0.0) for a in actions]
         q_max = max(q_vals)
-        greedy_actions = [q for a, q in zip(actions, q_vals) if q == q_max]
-        return actions[rng.integers(len(greedy_actions))]
+        greedy_actions = [a for a, q in zip(actions, q_vals) if q == q_max]
+        return greedy_actions[rng.integers(len(greedy_actions))]
 
 class DynaQ:
     def __init__(self, rng, env, epsilon, gamma, alpha, n):
         self.rng = rng
         self.env = env
-        self.epsilon = epsilon
         self.gamma = gamma 
         self.alpha = alpha
         self.n = n # represents iterations of planning per step (not t-steps)
 
         self.Q = {}
-        self.model = {}
-        self.b = EpsilonGreedy(self.env, self.epsilon)
+        self.model = {} # model[s][a] = (r, s_prime, done)
+
+        self.behavior = EpsilonGreedy(self.env, epsilon)
     
-    def q_max(self, s, Q):
+    def q_max(self, s):
         actions = list(self.env.actions(s))
-        if not actions: raise ValueError(f"No actions for state: {s}")
+        if not actions: return 0.0
 
-        q_vals = [Q.setdefault(s, {}).get(a, 0.0) for a in actions]
-        q_max = max(q_vals)
-        greedy_actions = [q for a, q in zip(actions, q_vals) if q == q_max]
-        selected_action = greedy_actions[self.rng.integers(len(greedy_actions))]
-        return self.q(s, selected_action)
-
-    def q(self, s, a):
+        return max(self.Q.get(s, {}).get(a, 0.0) for a in actions)
+    
+    def update_q(self, s, a, r, s_prime, done):
+        target = r if done else r + self.gamma * self.q_max(s_prime)
         self.Q.setdefault(s, {})
         self.Q[s].setdefault(a, 0.0)
-        return self.Q[s][a]
+        self.Q[s][a] += self.alpha * (target - self.Q[s][a])
     
-    def model(self, s, a, r, s_prime):
+    def update_model(self, s, a, r, s_prime, done):
         self.model.setdefault(s, {})
-        self.model[s].setdefault(a, {})   
-        self.model[s][a].setdefault((r, s_prime), (None, None))
-        return self.model[s][a]
-     
-    def run(self, epsiodes):
+        self.model[s][a] = (r, s_prime, done)
+
+    def planning_step(self):
+        if not self.model:
+            return
+
+        for _ in range(self.n):
+            states = list(self.model.keys())
+            s = states[self.rng.integers(len(states))]
+
+            actions_taken_in_s = list(self.model[s].keys())
+            a = actions_taken_in_s[self.rng.integers(len(actions_taken_in_s))]
+
+            r, s_prime, done = self.model[s][a]
+            self.update_q(s, a, r, s_prime, done)
+
+    def run(self, episodes):
         # info = []
-        for ep in epsiodes:
+        for _ in range(episodes):
             s_t, reset_info = self.env.reset(self.rng)
+            done = False
             # info.append(reset_info)
-            while True:
-                a_t = self.b.sample(self.rng, s_t, self.Q)
+
+            while not done:
+                a_t = self.behavior.sample(self.rng, s_t, self.Q)
 
                 s_tp1, r_tp1, terminated, truncated, step_info = self.env.step(s_t, a_t)
                 # info.append(step_info)
                 done = terminated or truncated
-                self.model(s_t, a_t, r_tp1, s_tp1)
 
-                q_sa = self.q(s_t, a_t)
-                self.Q[s_t][a_t] += self.alpha * (r_tp1 + self.gamma * self.q_max(s_tp1, self.Q) - q_sa)
+                self.update_q(s_t, a_t, r_tp1, s_tp1, done)
+                self.update_model(s_t, a_t, r_tp1, s_tp1, done)
+                self.planning_step()
+
                 s_t = s_tp1
-                if done: break
 
-
-
-
+        return self.Q
--- tabular/environments/ch_8_dyna_maze.py
+++ tabular/environments/ch_8_dyna_maze.py
@@ -1,11 +1,49 @@
-'''
-actions: up, down, left, right (deterministic)
-states: 
--47 total
--6 rows x 9 cols
--blockage states = ((2, 3, 4), 3), (5, 6), ((1, 2, 3), 8)
--starting state: (3, 1)
--goal state: (1, 9)
+class DynaMaze:
+    def __init__(self):
+
+        self.rows, self.cols = 6, 9
+        self.null_states = [(1, 2), (2, 2), (3, 2), (4, 5), (0, 7), (1, 7), (2, 7)]
+        self.start = (2, 0)
+        self.goal = (0, 8)
+
+        self._actions = [
+            (-1, 0), # up
+            ( 1, 0), # down
+            ( 0,-1), # left
+            ( 0, 1) # right
+        ]
+
+        self._states = [
+            (r, c)
+            for r in range(self.rows)
+            for c in range(self.cols)
+            if (r, c) not in self.null_states
+        ]
+
+        self.curr_state = self.start
+    
+    def actions(self, s=None): return self._actions
+    
+    def states(self): return self._states
+
+    def reset(self, seed=None):
+        self.curr_state = self.start
+        return self.curr_state, {}
+
+    def step(self, a):
+        r = 0
+        row, col = self.curr_state
+        dr, dc = a
+
+        r_prime = min(self.rows - 1, max(0, r + dr))
+        c_prime = min(self.cols - 1, max(0, c + dc))
+        
+        s_prime = (r_prime, c_prime)
+
+        if s_prime == self.goal: r = 1
+        if not s_prime in self.null_states: self.curr_state = s_prime
+        terminated = (s_prime == self.goal)
+
+        return self.curr_state, r, terminated, False, {}
 
 
-'''
